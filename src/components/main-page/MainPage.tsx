0ad06f610264702b03a1f606515337e12c5d9d21
BarinDwalin <allandnothingawesomeness@gmail.com>
2025-03-13 01:56:54 +0500

    split banner svg

--- src/components/main-page/MainPage.tsx
+++ src/components/main-page/MainPage.tsx
@@ -3,24 +3,48 @@ import "./MainPage.css";
 import { ReactComponent as BannerImage } from "../../images/banner.svg";
 import { Map } from "./Map";
 
+function Banner() {
+  return (
+    <div
+      style={{
+        display: "block",
+        width: "100%",
+        height: "initial",
+        margin: "auto",
+        maxWidth: "920px",
+        position: "relative",
+      }}
+    >
+      <img
+        src="/images/banner-meeple.png"
+        loading="lazy"
+        style={{
+          position: "absolute",
+          width: "50%",
+          left: "22%",
+          top: "17%",
+        }}
+      />
+      <SvgIcon
+        component={BannerImage}
+        className="banner"
+        inheritViewBox
+        sx={{
+          display: "block",
+          width: "100%",
+          height: "initial",
+        }}
+      />
+    </div>
+  );
+}
+
 export function MainPage() {
   return (
     <div className="page">
       {/* <h1 className="page-header">Добро пожаловать, авантюрист</h1> */}
-
+      <Banner></Banner>
       <div className="content">
-        <SvgIcon
-          component={BannerImage}
-          className="banner"
-          inheritViewBox
-          sx={{
-            display: "block",
-            width: "100%",
-            height: "initial",
-            margin: "auto",
-            maxWidth: "920px",
-          }}
-        />
         <Box
           sx={{
             margin: "0 24px",
